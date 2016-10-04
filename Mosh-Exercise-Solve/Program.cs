7a63357893cafc24224ef8f67c6ce77397346379
rasel01 <raseluiu.ac.bd@gmail.com>
2016-10-04 17:06:35 +0600

    excersice 1

--- Mosh-Exercise-Solve/Program.cs
+++ Mosh-Exercise-Solve/Program.cs
@@ -12,19 +12,66 @@ namespace Mosh_Exercise_Solve
     {
         static void Main(string[] args)
         {
+            // task : find differece between end time and start time
+            //Stopwatch sw = new Stopwatch();
+            //while (true)
+            //{
+            //    string start = sw.start();
+            //    Console.WriteLine("start :" + start);
+            //    Thread.Sleep(5000);
+            //    string end = sw.stop();
+            //    Console.WriteLine("stop :" + end);
+            //    TimeSpan ts = (TimeSpan.Parse(end) - TimeSpan.Parse(start));
+            //    Console.WriteLine("diffrence " + ts + Environment.NewLine);
 
-            Stopwatch sw = new Stopwatch();
-            while (true)
-            {
-                string start = sw.start();
-                Console.WriteLine("start :" + start);
-                Thread.Sleep(5000);
-                string end = sw.stop();
-                Console.WriteLine("stop :" + end);
-                TimeSpan ts = (TimeSpan.Parse(end) - TimeSpan.Parse(start));
-                Console.WriteLine("diffrence " + ts + Environment.NewLine);
-            }
+
+
+            //}
+
+            // task : exception occur when to stat called twice in a row
+
+
+            Post p = new Post();
+            int vote = 0;
+            Console.WriteLine(p.UploadPost());
+            //p.UploadPost();
+            bool count = true;
+                while (count)
+                {
+
+                    Console.WriteLine("Like This Post?" + " y or n ");
+                    string input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "y":
+                            vote = p.UpVote(vote);
+                            break;
+
+                        case "n":
+                            vote = p.DownVote(vote);
+                            break;
+                        default:
+                            vote = 0;
+                            break;
+
+                    }
+                    Console.WriteLine("want to vote more ?  y/n" + Environment.NewLine);
+                    var readLine = Console.ReadLine();
+                    if (readLine != null && readLine.Equals("n"))
+                    {
+                        count = false;
+                    }
+
+                
+                }
+                
+                
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Your post get " + vote + " vote ");
             
+                Console.ReadKey();
+
 
         }
     }

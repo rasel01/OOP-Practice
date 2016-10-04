93ebff7abde11335d938ed607edbf9d4f619fe0f
rasel01 <raseluiu.ac.bd@gmail.com>
2016-10-04 08:54:28 +0600

    OOP Practice

---
+++ Mosh-Exercise-Solve/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mosh_Exercise_Solve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch sw = new Stopwatch();
+            //Console.WriteLine(sw.start() - sw.stop());
+            while (true)
+            {
+                string start = sw.start();
+                Console.WriteLine("start :" + start);
+                Thread.Sleep(5000);
+                string end = sw.stop();
+                Console.WriteLine("stop :" + end);
+                TimeSpan ts = (TimeSpan.Parse(end) - TimeSpan.Parse(start));
+                Console.WriteLine("diffrence " + ts + Environment.NewLine);
+            }
+            
+
+        }
+    }
+}
---
+++ Mosh-Exercise-Solve/Stopwatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh_Exercise_Solve
+{
+    public class Stopwatch
+    {
+        public string start()
+        {
+            string start = DateTime.Now.ToString("HH:mm:ss ");
+            return start;
+        }
+
+        public string stop()
+        {
+            string stop = DateTime.Now.ToString("HH:mm:ss ");
+            return stop;
+        }
+    }
+}

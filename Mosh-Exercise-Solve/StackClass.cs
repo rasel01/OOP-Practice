fed8a253ebd2d32b1bfd15ecd888658b63efb5b4
rasel01 <raseluiu.ac.bd@gmail.com>
2016-10-05 11:32:15 +0600

    wegrfgd

---
+++ Mosh-Exercise-Solve/StackClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh_Exercise_Solve
+{
+    public class StackClass
+    {
+        List<object> objects = new List<object>();
+        public void Push(object obj)
+        {
+            objects.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+           object obj = objects.LastOrDefault();
+           return obj;
+        }
+
+        public void Clear()
+        {
+            objects.Clear();
+        }
+
+    }
+}

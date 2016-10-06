3e701811aec7afaaaed3393361775d8ff66b1f16
rasel01 <raseluiu.ac.bd@gmail.com>
2016-10-06 08:01:01 +0600

    db command class include

---
+++ Mosh-Exercise-Solve/DbCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh_Exercise_Solve
+{
+    public class DbCommand
+    {
+        public string Command { get; set; }
+        private readonly DbConnection _db;
+        public DbCommand(string instruction)
+        {
+            _db = new OracleConnection("ConnectinString For SQl Database connection");
+            Command = instruction;
+
+        }
+
+        public void Execute()
+        {
+            _db.ConnectionOpen();
+            Console.WriteLine(Command);
+            _db.ConnectionClose();
+        }
+    }
+}

22b9c8963f42ae259dcad14a4bf39107781e6b0d
rasel01 <raseluiu.ac.bd@gmail.com>
2016-10-06 09:03:18 +0600

    workflow

---
+++ Mosh-Exercise-Solve/WorkFlow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosh_Exercise_Solve
+{
+    public class WorkFlow
+    {
+        public Object Type { get; set; }
+    }
+}
